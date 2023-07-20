a9bcb1f63a65b1f16fb11e334bd22eeecfaa554e
GermanLitvin <96744232+GermanLitvin@users.noreply.github.com>
2023-07-20 18:33:15 +0300

    Add files via upload

---
+++ core.py
@@ -0,0 +1,97 @@
+from pprint import pprint
+from datetime import datetime
+from vk_api.exceptions import ApiError
+from config import acces_token
+
+import vk_api
+
+# получение данных о пользователе
+class VkTools:
+    def __init__(self, acces_token):
+        self.vkapi = vk_api.VkApi(token=acces_token)
+
+    #Вычисление возраста.
+    def _bdate_toyear(salf, bdate):
+        user_year = bdate.split('.')[2]
+        now = datetime.now().year
+        return now - int(user_year)
+    
+    #Вычисление параметров.
+    def get_profile_info(self, user_id):
+        try:
+            info, = self.vkapi.method('users.get',
+                                        {
+                                        'user_id': user_id,
+                                        'fields': 'city,sex,bdate,relation,bdate'
+                                        }
+                                        )           
+        except ApiError as e:
+            info ={}
+            print(f'Хьюстон, у нас проблемы = {e}.')
+
+        result = {'name' : (info ['first_name'] + ' ' + info ['last_name']) if 
+                  'first_name' in info and 'last_name' in info else None,
+                  'sex' : info.get('sex'),
+                  'city' : info.get('city')['title'] if info.get('city') is not None else None,
+                  'year' : self._bdate_toyear(info.get('bdate'))
+                  }
+        return result
+    
+    #Вычисление параметров.
+    def search_worksheet(self,params,offset):
+        try:
+            users = self.vkapi.method('users.search',
+                                {'count': 50,
+                                 'offset': offset,
+                                 'hometown':params['city'],
+                                 'sex': 1 if params['sex'] == 2 else 2,
+                                 'has_photo': True,
+                                 'age_from': params['year'] -3,
+                                 'age_to': params['year'] +3,
+                                 }
+                                 )
+        except ApiError as e:
+            user =[]
+            print(f'Хьюстон, у нас проблемы = {e}.')
+
+        #Причесали поиск.
+        result = [{'name': item['first_name']+ item['last_name'],
+                    'id': item['id']
+                    } 
+                for item in users['items'] if item['is_closed'] is False
+                 ]       
+        return result
+
+    #Поиск фото.
+    def get_photos(self, id):
+        try:
+            photos = self.vkapi.method('photos.get',
+                                {'owner_id': id,                                 
+                                 'album_id': 'profile',
+                                 'extended': 1
+                                 }
+                                 )
+        except ApiError as e:
+            photos ={}
+            print(f'Хьюстон, у нас проблемы = {e}.')
+
+        #Параметры фото.
+        result = [{'owner_id': item['owner_id'],
+                   'id': item['id'],
+                   'likes': item['likes']['count'],
+                   'comments': item['comments']['count']
+                    } for item in photos['items']
+                   ]
+        
+        #Здесь сортируем по лайкам и коментариям. 
+        result.sort(key=lambda x: (x['likes'] + x['comments']), reverse=True)
+        return result[:3]
+        
+if __name__ == '__main__':
+    user_id = "..."
+    tools = VkTools(acces_token)
+    params = tools.get_profile_info(user_id)
+    worksheets = tools.search_worksheet(params,10)
+    worksheet = worksheets.pop()
+    photos = tools.get_photos(worksheet['id'])
+    pprint(worksheets)
---
+++ interface.py
@@ -0,0 +1,89 @@
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from config import comunity_token, acces_token
+from core import VkTools
+from data_store import check_user, add_user, engine
+
+import vk_api
+
+
+class BotInterface():
+    def __init__(self, comunity_token, acces_token):
+        self.vk = vk_api.VkApi(token=comunity_token)
+        self.longpoll = VkLongPoll(self.vk)
+        self.vk_tools = VkTools(acces_token)
+        self.params = {}
+        self.worksheets = []
+        self.keys = []
+        self.offset = 0
+
+    def message_send(self, user_id, message, attachment=None):
+        self.vk.method('messages.send',
+                       {'user_id': user_id,
+                        'message': message,
+                        'attachment': attachment,
+                        'random_id': get_random_id()}
+                       )
+
+    def photos_for_send(self, worksheet):
+        photos = self.vk_tools.get_photos(worksheet['id'])
+        photo_string = ''
+        for photo in photos:
+            photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+        return photo_string
+
+#Добавление и проверка БД.
+    def get_profile(self, worksheets, event):
+        while True:           
+            if worksheets:
+                worksheet = worksheets.pop()
+                if not check_user(engine, event.user_id, worksheet['id']):
+                    add_user(engine, event.user_id, worksheet['id'])
+                    yield worksheet
+            else:
+                worksheets = self.vk_tools.search_worksheet(
+                    self.params, self.offset)
+
+# обработка событий / получение сообщений
+    def event_handler(self):
+        for event in self.longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                if event.text.lower() == 'привет':
+
+                    '''Логика для получения данных о пользователе'''
+                    self.params = self.vk_tools.get_profile_info(event.user_id)
+                    self.message_send(
+                        event.user_id, f'Приветствую тебя {self.params["name"]} в это непростое время.')               
+                    self.keys = self.params.keys()
+                    for inf in self.keys:
+                        if self.params[inf] is None:
+                            self.params[inf] = self(event)
+                    self.message_send(event.user_id, 'Напиши "Поиск" и ты найдешь себе пару.')
+                elif event.text.lower() == 'поиск':
+
+                    #Логика для поиска анкет
+                    try:
+                        self.message_send(
+                            event.user_id, 'Активация поиска.')
+                        research = next(iter(self.get_profile(self.worksheets, event)))
+                        if research:
+                            photo_string = self.photos_for_send(research)
+                            self.offset += 50
+                            self.message_send(
+                                event.user_id,
+                                f'Пользователь: {research["name"]}. \n Аккаунт id{research["id"]}.',
+                                attachment=photo_string
+                            )
+                    except:
+                        self.message_send(event.user_id, 'Постой, а где привет? :)')
+                elif event.text.lower() == 'пока':
+                    self.message_send(
+                        event.user_id, 'До новых встреч.')
+                else:
+                    self.message_send(
+                        event.user_id, 'Поприветствуй нас для начала знакомства с нами и следуй подсказкам. :)')
+
+
+if __name__ == '__main__':
+    bot_interface = BotInterface(comunity_token, acces_token)
+    bot_interface.event_handler()
